db7d47a0d17d64d5f95690fda1efccd3f280c6ec
Rodrigo <ronico041012@gmail.com>
2020-10-20 16:56:17 -0300

    Updates

---
+++ precache-manifest.87de2fdbcf4267aa43b209bda535dab2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7da0e2d6e8b2699ad5ea9cddb993d015",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "85a032d5725571bec399",
+    "url": "/todo-app/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "6d98d81ec41a9f2b85c6",
+    "url": "/todo-app/static/css/main.96e43937.chunk.css"
+  },
+  {
+    "revision": "85a032d5725571bec399",
+    "url": "/todo-app/static/js/2.8df37360.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todo-app/static/js/2.8df37360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d98d81ec41a9f2b85c6",
+    "url": "/todo-app/static/js/main.931e98c9.chunk.js"
+  },
+  {
+    "revision": "3d6144395c4d7741f4ff",
+    "url": "/todo-app/static/js/runtime-main.2da2c702.js"
+  }
+]);
